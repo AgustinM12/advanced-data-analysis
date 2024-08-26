cec0e43f168eb102dd09e4b52b965ac89bb8d5af
Agustin.M12 <agustinmazza12345@gmail.com>
2024-08-26 16:04:46 -0300

    feat: :sparkles: se implementaron el analisis de los datos de la db y los graficos

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from db.db import DataBaseManager
+from analisis.data_analisis import DataAnalyzer 
+from graphics.graphics import DataVisualizer
 from df_pandas import read_csv
+import pandas as pd
 
 # * Configuración de la conexión
 host = 'localhost'
@@ -29,25 +32,53 @@ db_manager.execute_query(create_table_query)
 # * lectura del archivo csv 
 csv = read_csv("data_employee.csv")
 
-# * Insertar datos
-insert_query = """
-    INSERT INTO EmployeePerformance (employee_id, department, performance_score, years_with_company, salary)
-    VALUES (%s, %s, %s, %s, %s)
-    """
+# * Verificar que ya no existan datos
+check_count_query = "SELECT COUNT(*) FROM EmployeePerformance"
+row_count = db_manager.fetch_all(check_count_query)[0][0]
 
-# ! Iterar sobre el DataFrame e insertar los registros
+# * Insertar datos si no hay registros aun
+if row_count == 0:
+    insert_query = """
+        INSERT INTO EmployeePerformance (employee_id, department, performance_score, years_with_company, salary)
+        VALUES (%s, %s, %s, %s, %s)
+        """
+    # ! Iterar sobre el DataFrame e insertar los registros
 
-for i, row in csv.iterrows():
-    # !  Extraer solo las columnas relevantes
-    data = (
-        row['employee_id'],
-        row['department'],
-        row['performance_score'],
-        row['years_with_company'],
-        row['salary']
-    )
-    db_manager.cursor.execute(insert_query, data)
+    for i, row in csv.iterrows():
+        # !  Extraer solo las columnas relevantes
+        data = (
+            row['employee_id'],
+            row['department'],
+            row['performance_score'],
+            row['years_with_company'],
+            row['salary']
+        )
+        db_manager.cursor.execute(insert_query, data)
+        
+     # * Confirmar los cambios
+    db_manager.conn.commit()
+    print("Se insertaron los registros de manera exitosa")
+else: 
+    print("Ya hay registros")
 
-    # ! Confirmar los cambios
-db_manager.conn.commit()
-print("Se insertaron los registros de manera exitosa")
+select_query = "SELECT * FROM EmployeePerformance"
+all_users = db_manager.fetch_all(select_query)
+
+
+# ! Crear un DataFrame a partir de los registros obtenidos
+column_names = ["id", "employee_id", "department", "performance_score", "years_with_company", "salary"]
+
+df = pd.DataFrame(all_users, columns=column_names)
+
+
+#  * Uso del analisis de datos
+analyzer = DataAnalyzer(df)
+analysis_results = analyzer.analyze()
+print("Resultado del analisis de los datos:", analysis_results)
+
+
+# * Visualizacion de graficos
+visualizer = DataVisualizer(df)
+visualizer.graphic_data1()
+visualizer.graphic_data2()
+visualizer.graphic_data3()
